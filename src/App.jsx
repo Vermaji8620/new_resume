082a0839c84f167d9177b46a867cbb6202a0f40f
vermaji <rajaditya14032002@gmail.com>
2024-07-31 21:21:08 +0530

    home page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
