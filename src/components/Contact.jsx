94ea69ff59d73f9092399626cfed85bd9cfae22c
vermaji <rajaditya14032002@gmail.com>
2024-08-01 11:13:26 +0530

    done adding framerr motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,17 @@
 import { CONTACT } from "../constants/index.js";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-      <h1 className="text-center text-4xl my-10">Get In Touch</h1>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="text-center text-4xl my-10"
+      >
+        Get In Touch
+      </motion.h1>
       <div className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.address}</p>
         <p className="my-4">{CONTACT.phoneNo}</p>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,40 @@
 import { EXPERIENCES } from "../constants/index.js";
+import { motion } from "framer-motion";
 
 const Experience = () => {
   return (
     <div className="border-neutral-900 pb-4 border-b ">
-      <h1 className="my-20 text-center text-4xl">Experience</h1>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Experience
+      </motion.h1>
       <div className="">
         {EXPERIENCES.map((experience, index) => (
           <div key={index} className="flex flex-wrap mb-8 lg:justify-center ">
-            <div className="w-full lg:w-1/4">
+            <motion.div
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              initial={{ opacity: 0, x: -50 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+              className="w-full lg:w-1/4"
+            >
               <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              initial={{ opacity: 0, x: 50 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
               <h6 className="mb-2 font-semibold">
                 {experience.role} -{" "}
                 <span className="text-sm text-purple-100">
@@ -30,7 +54,7 @@ const Experience = () => {
                   </div>
                 ))}
               </div>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
